7ab448638f6fff866d21e49f01ddcecd14b33a27
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-13 14:40:13 +0200

    Create unit test for item business service using @MockBean

--- src/test/java/com/learn/unittesting/controllers/ItemControllerTest.java
+++ src/test/java/com/learn/unittesting/controllers/ItemControllerTest.java
@@ -1,14 +1,18 @@
 package com.learn.unittesting.controllers;
 
+import com.learn.unittesting.business.ItemBusinessService;
+import com.learn.unittesting.models.Item;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,9 +20,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ItemControllerTest {
 
     private static final String ITEM_URL = "/item";
+    private static final String ITEM_BUSINESS_URL = "/item-from-business";
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ItemBusinessService itemBusinessService;
 
     @Test
     public void getItem() throws Exception {
@@ -32,6 +40,23 @@ class ItemControllerTest {
                         "quantity: 22,\n" +
                         "price: 2000\n" +
                         "}"))
+                .andReturn();
+    }
+
+    @Test
+    public void getItemFromBusiness() throws Exception {
+        // prepare itemBusiness Service
+        when(itemBusinessService.getItem())
+                .thenReturn(new Item(1, "TV", "LG", 22, 2000));
+
+        // Call GET '/item' accept application/json
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get(ITEM_BUSINESS_URL)
+                .accept(MediaType.APPLICATION_JSON_VALUE);
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(content().json("{name: 'TV', price: 2000}"))
                 .andReturn();
     }
 }
