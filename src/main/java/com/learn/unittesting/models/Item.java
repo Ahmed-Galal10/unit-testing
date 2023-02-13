fba5b57469e9207ab8e904ceaaed8ecacac02899
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-13 15:44:36 +0200

    Configure h2-console & customize item model to be an @Entity

--- src/main/java/com/learn/unittesting/models/Item.java
+++ src/main/java/com/learn/unittesting/models/Item.java
@@ -1,6 +1,18 @@
 package com.learn.unittesting.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Item {
+    @Id
     int id;
     String name;
     String brand;
@@ -12,46 +24,6 @@ public class Item {
         this.name = name;
         this.brand = brand;
         this.quantity = quantity;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
         this.price = price;
     }
 }
