e1fba479ca4d3e9ee078c2888c5238ec46b51e32
Ahmed Galal <AhmedGalal10895@Gmail.com>
2023-02-17 03:44:39 +0200

    Try AssertJ library usages in testing

---
+++ src/test/java/com/learn/unittesting/spikes/AssertJTest.java
@@ -0,0 +1,28 @@
+package com.learn.unittesting.spikes;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class AssertJTest {
+
+    @Test
+    public void applyHamcrestHelpers() {
+        List<Integer> numbers = Arrays.asList(8, 15, 22);
+
+        assertThat(numbers).hasSize(3)
+                .contains(15, 8)
+                .allMatch(x -> x > 5)
+                .noneMatch(x -> x > 25);
+
+        assertThat("Ahmed").isNotNull().
+                containsIgnoringCase("ahm")
+                .startsWith("Ah")
+                .endsWith("ed");
+    }
+}
